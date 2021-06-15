c7602109efe5c65e5ab53ba324b140f8ce558bb8
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-06-14 19:08:59 -0700

    implements traditional method

--- models/gift-exchange.js
+++ models/gift-exchange.js
@@ -3,11 +3,11 @@ const { get } = require("../routes/gift-exchange");
 class GiftExchange {
     static async pairs (names) {
         //Pairs people together and returns array of arrays
-        let pairArr = [];
+        const pairArr = [];
         
         while (names.length > 0) {
             let index = await this.getRandomNumber(0, names.length);
-            let tempArr = [];
+            const tempArr = [];
             tempArr.push(names[index]);
             names.splice(index, 1);
 
@@ -38,8 +38,15 @@ class GiftExchange {
         //"they is giving a gift to us",
         //"us is giving a gift to me",
         //returns array of strings
-        const playerPairs = this.pairs(names);
-        
+        const playerPairs = await this.pairs(names);
+
+        const pairPhraseArr = [];
+        for (let index = 0; index < playerPairs.length; index++) {
+
+            pairPhraseArr.push(`${playerPairs[index][0]} is giving a gift to ${playerPairs[index][1]}`);
+            pairPhraseArr.push(`${playerPairs[index][1]} is giving a gift to ${playerPairs[(index + 1) % 3][0]}`);
+        }
+        return pairPhraseArr;
     }
 }
 
